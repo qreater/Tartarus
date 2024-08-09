2bfced9f1193402fd830029995cdcc977a87cdc1
Vaishnavi Manicka <76160783+Vaish24navi@users.noreply.github.com>
2024-08-10 00:27:59 +0530

    feat(utils): database utilities for API building (#2)

---
+++ tests/integration_tests/test_datasource.py
@@ -0,0 +1,44 @@
+import pytest
+from app.utils.data.datasource import DataStore
+from app.utils.config.conf import settings
+
+
+class TestDataStoreIntegration:
+    """
+    Integration test suite for the DataStore class.
+    """
+
+    @pytest.fixture(scope="class")
+    def datastore(self):
+        """
+        Fixture to initialize the DataStore instance for integration tests.
+        Ensures that the connection is properly established before tests and
+        cleaned up after all tests in the class have run.
+        """
+        ds = DataStore()
+        yield ds
+        del ds
+
+    def test_connection_established(self, datastore):
+        """
+        Test that the DataStore connection is successfully established.
+
+        This test verifies that a connection is established and that it is
+        not None, indicating that the database connection is functional.
+        """
+        assert datastore.connection is not None
+
+    def test_create_database(self, datastore):
+        """
+        Test that the database is created if it does not exist.
+
+        This test verifies that the DataStore class creates the database
+        if it is not already present. It checks for the existence of the
+        database by querying the PostgreSQL catalog.
+        """
+        cursor = datastore.connection.cursor()
+        cursor.execute(
+            f"SELECT * FROM pg_catalog.pg_database WHERE datname = '{settings.DB_NAME}' LIMIT 1;"
+        )
+        exists = cursor.fetchone()
+        assert exists is not None
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,10 @@ from main import app
 client = TestClient(app)
 
 
-def test_read_root():
+def test_startup():
+    """
+    Test to verify that the FastAPI application starts up successfully.
+    """
     response = client.get("/")
     assert response.status_code == 200
     assert response.json()["message"] == "Welcome to Tartarus API"

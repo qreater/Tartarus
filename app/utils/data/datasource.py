f8bd321f917f858343fa11672c781d112dc3466a
niyasrad <niyasrad@gmail.com>
2024-08-10 13:51:39 +0530

    ci(pre-commit): licensing, code linting

--- app/utils/data/datasource.py
+++ app/utils/data/datasource.py
@@ -1,3 +1,11 @@
+"""
+
+ Copyright 2024 @Qreater
+ Licensed under the Apache License, Version 2.0.
+ See: http://www.apache.org/licenses/LICENSE-2.0
+
+"""
+
 from psycopg2 import connect
 from app.utils.config.conf import settings
 import logging
